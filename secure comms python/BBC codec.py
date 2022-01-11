9ef49aafd54f84ee11bdf1f37d8875613a2854ee
James Morrison <jamescmorrison00@gmail.com>
2022-01-11 14:27:40 -0700

    Initial Commit

---
+++ secure comms python/BBC codec.py
@@ -0,0 +1,11 @@
+# Author: James C Morrison
+# Date: 1/1//2022
+# Project: ECE-499 Secure Communications
+# 
+# Description:
+# Encode and decode a message using the BBC algorithm as described by 
+# Bahn, Baird, and Carlisle. The BBC algorithm is an unkeyed approach
+# to jamming resistance. Without the use of keys, omnidirectional
+# communication may be achieved with relatively low power and high
+# speed. 
+
---
+++ secure comms python/glowworm.py
@@ -0,0 +1,8 @@
+# Author: James C Morrison
+# Date: 1/1//2022
+# Project: ECE-499 Secure Communications
+# 
+# Description:
+# The Glow Worm Hash and supporting functions to be used with for
+# mark placement in the BBC codec.
+
