ad32bd514cefa565272bf807122720f199ce17ff
James Morrison <jamescmorrison00@gmail.com>
2022-01-19 21:43:50 -0700

    Initial Commit

---
+++ secure comms python/test.py
@@ -0,0 +1,17 @@
+mylist=['a','b','c','d','e','f','g','h','i']
+newlist = mylist[:-2]
+print(newlist)
+print(1<<2)
+if 0:
+    print("it evaluated to true")
+
+print([n for n in range(3)])
+print([1 for n in range(0)])
+
+print("test commmencing")
+item = [0, 1, 2]
+for n in range(len(item)):
+        # Build out the hash for each substring
+        for j in range(n+1):
+            print(item[j])
+        print()
