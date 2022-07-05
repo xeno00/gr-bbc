c6cffd427dba9bd0b0ea65419254761e36cf17f5
Neil Rogers <livethisdream@gmail.com>
2022-07-05 14:59:20 -0600

    made MSG_LEN and COD_LEN class properties for the encoder and decoder. Also, tweaked test.py to have a bit more granularity for debugging.

--- secure comms python/BBCCodecOO.py
+++ secure comms python/BBCCodecOO.py
@@ -7,30 +7,36 @@
 # CURRENT MODEL
 #from math import ceil
 import glowworm as gw
-MSG_LEN = 2**10
-COD_LEN = 2**20
+#MSG_LEN = 2**3  # bits  # now a class property
+#COD_LEN = 2**6  # bits  # now a class property
 DEFAULT_CHECKSUM = 0
+
+#global COD_LEN
 
 class Codec:
     # The codec is comprised of an encoder and decoder, with an associated message/codeword pair
-    def __init__(self):
-        self.encoder = Encoder()
-        self.decoder = Decoder()
+    def __init__(self, MSG_LEN, COD_LEN):
+        self.MSG_LEN = MSG_LEN
+        self.COD_LEN = COD_LEN
+        self.encoder = Encoder(self.MSG_LEN, self.COD_LEN)
+        self.decoder = Decoder(self.MSG_LEN, self.COD_LEN)
 
     # Resulting functionality should be "mycodec.encode("My password is xwing")"
     def bbc_encode(self, message):
-        self.encoder = Encoder()
+        self.encoder = Encoder(self.MSG_LEN, self.COD_LEN)
         return self.encoder.encode(message)
 
     # Resulting functionality should be "mycodec.decode(bytearray1)"
     def bbc_decode(self, codeword):
-        self.decoder = Decoder()
+        self.decoder = Decoder(self.MSG_LEN, self.COD_LEN)
         return self.decoder.decode(codeword)
 
 
 class Encoder:
-    def __init__(self):
+    def __init__(self, MSG_LEN, COD_LEN):
         self.shift_register = self.init_shift_register()
+        self.MSG_LEN = MSG_LEN
+        self.COD_LEN = COD_LEN
 
     def init_shift_register(self):
         shift_register = [0 for i in range(32)]
@@ -39,26 +45,28 @@ class Encoder:
 
     def parse_input(self, input):
         input = input.encode(encoding="ASCII")
-        message = bytearray(int(MSG_LEN/8))
+        message = bytearray(int(self.MSG_LEN/8))
         memoryview(message)[0:(len(input))] = input
         return message
 
     def encode(self, input):
         message = self.parse_input(input)
-        codeword = bytearray(int(COD_LEN/8))
-        for i in range(MSG_LEN):
+        codeword = bytearray(int(self.COD_LEN/8))
+        for i in range(self.MSG_LEN):
             element = memoryview(message)[int((i-i%8)/8)]
             bit = ((element) >> (i%8)) & 0b1
-            mark_loc = gw.add_bit(bit, self.shift_register) % COD_LEN
+            mark_loc = gw.add_bit(bit, self.shift_register) % self.COD_LEN
             memoryview(codeword)[int((mark_loc-mark_loc%8)/8)] |= 1<<(mark_loc%8)
         return(codeword)
 
 
 class Decoder:
-    def __init__(self):
+    def __init__(self, MSG_LEN, COD_LEN):
         self.message_list = []
         self.num_checksum = DEFAULT_CHECKSUM
         self.shift_register = self.init_shift_register()
+        self.MSG_LEN = MSG_LEN
+        self.COD_LEN = COD_LEN
 
     def init_shift_register(self):
         shift_register = [0 for i in range(32)]
@@ -67,23 +75,24 @@ class Decoder:
     
     def decode(self, codeword):
         self.message_list = []
-        message = bytearray(MSG_LEN)
+        message = bytearray(int(self.MSG_LEN/8)) # bits to bytes
         self._decode_BBC_recursive(message, codeword, 0) # make the first recursive call to decode
         return self.message_list
     
     def _decode_BBC_recursive(self, message, codeword, index):
-        if index == (MSG_LEN-1):
-                self.message_list.append(bytes(memoryview(message)[0:MSG_LEN-1-self.num_checksum]).decode(encoding='ascii'))
+
+        if index == (self.MSG_LEN-1):
+                self.message_list.append(bytes(memoryview(message)[0:self.MSG_LEN-1-self.num_checksum]).decode(encoding='ascii'))
         else:
             # assuming the next message bit is a 0, check for a mark in the codeword
-            val = (gw.add_bit(0, self.shift_register) % (COD_LEN))
+            val = (gw.add_bit(0, self.shift_register) % (self.COD_LEN))
             bit = ((memoryview(codeword)[int((val-val%8)/8)])>>(val%8))& 0b1
             if bit == 1:
                 self._decode_BBC_recursive(message, codeword, index+1)
             gw.del_bit(0, self.shift_register)
 
             # assuming the next message bit is a 1, check for a mark in the codeword
-            val = (gw.add_bit(1, self.shift_register) % (COD_LEN))
+            val = (gw.add_bit(1, self.shift_register) % (self.COD_LEN))
             bit =  (memoryview(codeword)[int((val-val%8)/8)]>>(val%8))& 0b1
             if bit == 1: #(1<<val) == (codeword & (1<<val)):
                 memoryview(message)[int((index-index%8)/8)] |= (1<<index%8)
--- secure comms python/test.py
+++ secure comms python/test.py
@@ -2,10 +2,29 @@ from msilib.schema import BBControl
 
 
 import BBCCodecOO as bbc
+import sys
 
-mycodec = bbc.Codec()
-s = "hello my name is james. I wrote this encoder in python. This codec uses the BBC encoding scheme. If I wanted to share keys, this is how I would do it."
-b = bytearray()
-b.extend(s.encode(encoding="ASCII"))
-print(mycodec.bbc_decode(mycodec.bbc_encode(s)))
+sys.setrecursionlimit(1028)
+#s = "hello my name is james. I wrote this encoder in python. This codec uses the BBC encoding scheme. If I wanted to share keys, this is how I would do it." # 150 bytes
+#s = "hello my name is james. I wrote this encoder in python. This codec uses the BBC encoding scheme to share keys." # 110 bytes
+s = "AB"
+MSG_LEN=len(s)*8  # needs to be in bits for BBCCodecOO.py
+COD_LEN = 2**7    # needs to be in bits for BBCCodecOO.py
+
+mycodec = bbc.Codec(MSG_LEN, COD_LEN)
+#b = bytearray()  # what is this for?
+#b.extend(s.encode(encoding="ASCII")) # what is this for?
+
+# Encode
+bbcencoded = mycodec.bbc_encode(s)
+
+# rearrange the codeword to be more visually appealing
+bbcencodedcorr =  ' '.join([format(bbcencoded[i],'#010b')[2:] for i in range(int(COD_LEN/8)-1,-1,-1)])
+print("Codeword is: " + str(bbcencodedcorr))
+
+#Decode
+bbcdecoded = mycodec.bbc_decode(bbcencoded)
+for msg in range(len(bbcdecoded)):
+    print("Possible decoded Message is: " + bbcdecoded[msg] )
+
 
