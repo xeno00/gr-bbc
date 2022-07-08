f0be4287370d6fc1c8ee5455b1331fcbdb5ae625
James Morrison <jamescmorrison00@gmail.com>
2022-07-07 21:11:43 -0400

    BBC Codec 4.4 - Added line 102 in OO, set bit low to support multiple messages

--- secure comms python/BBCCodecOO.py
+++ secure comms python/BBCCodecOO.py
@@ -99,3 +99,4 @@ class Decoder:
                 memoryview(message)[int((index-index%8)/8)] |= (1<<index%8)
                 self._decode_BBC_recursive(message, codeword, index+1)
             gw.del_bit(1, self.shift_register)
+            memoryview(message)[int((index-index%8)/8)] = 0
