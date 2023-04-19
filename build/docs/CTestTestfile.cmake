117ec10b60c0b2168f8bd820f8fa8126deddc133
James <jamescmorrison00@gmail.com>
2023-04-20 04:51:57 +1200

    Delete build directory

--- build/docs/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/james/git/gr-bbc/docs
-# Build directory: /home/james/git/gr-bbc/build/docs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("doxygen")
--- build/include/bbc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/james/git/gr-bbc/include/bbc
-# Build directory: /home/james/git/gr-bbc/build/include/bbc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
